36e756d6ede0163f0472ae72a2626468e5f3e46e
MatthewC221 <matthewchen42@gmail.com>
2017-08-01 14:59:48 +1000

    Sped up FFT

--- DFT.h
+++ DFT.h
@@ -2,18 +2,23 @@
 #include <complex.h>
 #include <stdlib.h>
 #include <math.h>
+#include <inttypes.h>
 
 // Will hopefully turn this into a lib for DFT and FFT in C
 
 #ifndef _DFT_h
 #define _DFT_h
 #define M_PI   3.14159265358979323846264338327950288
+#define POW 20
 
 double complex * DFT (double complex * arr, size_t DFT_size);
 double complex * FFT (double complex * arr, size_t DFT_size);
 double complex sum(double complex * arr, int index, int DFT_size);
 int nextPow(int size);
 void printSignal (double complex * arr, size_t DFT_size);
+
+int powers_of_2[20] = {2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 
+    2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 2097152};
 
 /*
 int main(int argc, char *argv[])
@@ -46,7 +51,15 @@ double complex *
 FFT (double complex *arr, size_t FFT_size)
 {
 
-    int padding_size = nextPow(FFT_size);
+    int padding_size = -1;
+    for (int i = 0; i < POW; i++) {
+        if (powers_of_2[i] > FFT_size) {
+            padding_size = powers_of_2[i];
+            break;
+        }
+    }
+    printf("padding size = %d\n", padding_size);
+    
     // Zero pad first
     double complex * padded = malloc(sizeof(double complex) * padding_size);
 
@@ -58,44 +71,26 @@ FFT (double complex *arr, size_t FFT_size)
 
     int half = padding_size / 2; 
 
-    double complex * even = malloc(sizeof(double complex) * half);
-    double complex * odd = malloc(sizeof(double complex) * half);
-
-    int count = 0;
-    for (int i = 0; i < padding_size; i = i + 2) {
-        even[count++] = padded[i];
-    }
-
-    count = 0;
-    for (int i = 1; i < padding_size; i = i + 2) {
-        odd[count++] = padded[i];
-    }
-
-    double complex * even_DFT = malloc(sizeof(double complex) * half);
-    double complex * odd_DFT = malloc(sizeof(double complex) * half);
-
     // NOT GOING TO LIE: This took me ages, I kept messing up the values... zero-pad but don't use
     // the size INCLUDING the pad. E.g. N = 8 even if there are 8 zero-pad vals
 
-    for (int j = 0; j < half; j++) {
-        double complex sum = 0;
-        for (int i = 0; i < half; i++) {
-            sum = sum + (even[i] * cexp(-I * 2 * M_PI * (i * 2) * j / FFT_size));
-        }
-        even_DFT[j] = sum;
-    }
+    double complex * overall_FFT = malloc(sizeof(double complex) * FFT_size);
 
-    for (int j = 0; j < half; j++) {
-        double complex sum = 0;
-        for (int i = 0; i < half; i++) {
-            sum = sum + (odd[i] * cexp(-I * 2 * M_PI * (i * 2 + 1) * j / FFT_size));
-        }
-        odd_DFT[j] = sum;
-    }
+    for (int j = 0; j < FFT_size; j++) {
 
-    double complex * overall_FFT = malloc(sizeof(double complex) * half);
-    for (int i = 0; i < half; i++) {
-        overall_FFT[i] = even_DFT[i] + odd_DFT[i];
+        double complex sum_odd = 0;
+        double complex sum_even = 0;
+
+        for (int i = 0; i < half; i++) {
+            int index = (i * 2) + 1;
+            sum_odd = sum_odd + (padded[index] * cexp(-I * 2 * M_PI * index * j / FFT_size));
+        }
+        for (int i = 0; i < half; i++) {
+            int index = i * 2;
+            sum_even = sum_even + (padded[index] * cexp(-I * 2 * M_PI * index * j / FFT_size));
+        }
+
+        overall_FFT[j] = sum_odd + sum_even;
     }
 
     return overall_FFT;
@@ -128,6 +123,15 @@ complex sum(double complex * arr, int index, int DFT_size)
     }
     
     return sum; 
+}
+
+double 
+complex * convert(uint8_t * arr, int size)
+{
+    double complex * new_arr = (double complex *)malloc(sizeof(double complex) * size);
+    for (int i = 0; i < size; i++) new_arr[i] = arr[i] + 0 * I;
+
+    return new_arr;
 }
 
 // Use later for padding
